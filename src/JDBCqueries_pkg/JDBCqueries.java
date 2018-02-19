a03ff25f81c97b226452711b99fec82ea16be3c9
Dave Kloosterman <Owner@DESKTOP-6AUTM45>
2018-02-19 10:25:52 -0500

    - initialize "Use Local Database" to true, configured in JDBSqueries.java - properly initialize the check in the box accordingly

--- src/JDBCqueries_pkg/JDBCqueries.java
+++ src/JDBCqueries_pkg/JDBCqueries.java
@@ -29,7 +29,7 @@ public class JDBCqueries {
     static final String USER_LOCAL = "root";
     static final String PASS_LOCAL = "rootMysql151";
 
-    static boolean useLocalDB = false;
+    static boolean useLocalDB = true;
     static boolean connectedToDB = false;
 //    static final boolean USE_LOCAL_DB = false;
 
